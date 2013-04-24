59f8d6c0875a9f720ff89525c0aa02d99522541b
Gordon L. Hempton <ghempton@gmail.com>
2013-04-24 16:42:59 -0700

    fix feed item body attribute

--- app/assets/javascripts/models/feed_item.js
+++ app/assets/javascripts/models/feed_item.js
@@ -1,8 +1,8 @@
 App.FeedItem = DS.Model.extend({
   title: DS.attr('string'),
   author: DS.attr('string'),
-  content: DS.attr('string'),
-  contentSnippet: DS.attr('string'),
+  body: DS.attr('string'),
+  bodySnippet: DS.attr('string'),
   link: DS.attr('string'),
   publishedDate: DS.attr('date'),
 
